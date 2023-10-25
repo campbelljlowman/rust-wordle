536604d70559b797382f6f094a7a2de3cd24bad5
Campbell Lowman <campbelllowman@gmail.com>
2023-10-25 19:27:57 -0400

    add guess and print status

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-struct WordleBoard {
-    wordle_word: str,
+#[allow(dead_code)]
+
+struct WordleBoard<'a> {
+    wordle_word: &'a str,
     is_solved: bool,
-    guesses: Vec<str>
+    guesses: Vec<&'a str>
 }
 
+impl <'a> WordleBoard<'a> {
+
+    fn new(wordle_word: &'a str) -> WordleBoard {
+        WordleBoard {
+            wordle_word,
+            is_solved: false,
+            guesses: Vec::new()
+        }
+    }
+
+    fn print_status(self) {
+        for guess in self.guesses {
+            println!("{}", guess)
+        }
+    }
+
+    fn add_guess(&mut self, new_guess: &'a str) {
+        self.guesses.push(new_guess)
+    }
+}
 
 fn main() {
-    let wordle_word = "hello"
+    let mut wordle_board = WordleBoard::new("hello");
+    wordle_board.add_guess("new_guess");
+    wordle_board.print_status();
 
-    println!("Hello, world!");
 }
